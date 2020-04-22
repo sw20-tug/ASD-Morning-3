fac75f93edc3183c76665632d43c7831e3cf8eec
Aleksandar Iliskovic <iliskovic@student.tugraz.at>
2020-04-22 08:45:10 +0200

    LANG_004 third prototype of the study Interface[FA,IA]

--- main_module/src/main/java/com/asdmorning3/basic/studyInterface.java
+++ main_module/src/main/java/com/asdmorning3/basic/studyInterface.java
@@ -1,4 +1,4 @@
-package com.asdmorning3.test;
+package com.asdmorning3.basic;
 
 import javax.swing.*;
 import javax.swing.event.*;
@@ -6,29 +6,43 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import java.io.*;
+import com.asdmorning3.basic.Vocable;
+import com.asdmorning3.basic.VocableDictionary;
+import com.asdmorning3.test.InterfaceLanguages;
+import junit.runner.Version;
 
 
-class studyInterface {
+
+
+public class studyInterface {
 
     static JFrame frame;
     private static JDialog d;
     static JList list;
     JLabel lblLang1;
     JLabel lblLang2;
+    InterfaceLanguages inter;
+    VocableDictionary dictionary;
 
-    public studyInterface() {
+    public studyInterface(VocableDictionary dictionary_) {
 
         frame = new JFrame("Study Interface");
         JPanel pane = new JPanel(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         frame.setSize(600, 600);
 
+        dictionary = dictionary_;
+        ArrayList<String> languages = new ArrayList<String>();
+        for (Vocable.Language language: Vocable.Language.class.getEnumConstants())
+        {
+            languages.add(language.name());
+        }
 
-        String[] languages = {"English", "German"};
+        //String[] languages = {"English", "German"};
         final String[] english = {"house", "dog", "cat", "mouse"};
         final String[] german = {"Haus", "Hund", "Katze", "Maus"};
-        final JComboBox fromLanguage = new JComboBox(languages);
-        final JComboBox toLanguage = new JComboBox(languages);
+        final JComboBox fromLanguage = new JComboBox(new DefaultComboBoxModel<String>(languages.toArray(new String[0])));
+        final JComboBox toLanguage = new JComboBox(new DefaultComboBoxModel<String>(languages.toArray(new String[0])));
         final JComboBox setAnotherLanguage;
 
         lblLang1 = new JLabel("From:");
@@ -65,6 +79,7 @@ class studyInterface {
         c.gridy = 5;
         list = new JList();
         JScrollPane scrollPane = new JScrollPane(list);
+
         pane.add(scrollPane, c);
 
         frame.add(pane);
@@ -74,34 +89,41 @@ class studyInterface {
             {
                 Object obj = toLanguage.getSelectedItem();
                 String currentLanguage = String.valueOf(obj);
-                //d = new JDialog(frame, currentLanguage);
-                //d.setSize(400, 400);
 
-                if(currentLanguage == "English") {
-                    list.setListData(english);
+                Object obj2 = fromLanguage.getSelectedItem();
+                String secondCurrentLanguage = String.valueOf(obj2);
+
+                if(currentLanguage != secondCurrentLanguage) {
+                    if (currentLanguage == "ENG") {
+                        list.setListData(english);
+                    }
+                    else if(currentLanguage == "GER") {
+                        list.setListData(german);
+                    }
                 }
                 else
-                    list.setListData(german);
+                    JOptionPane.showMessageDialog(frame,
+                            "Select two different languages!",
+                            "WARNING",
+                            JOptionPane.WARNING_MESSAGE);
 
-                //pane.add(list, c);
-
-                //d.add(list);
-                //d.setVisible(true);
             }
 
         });
 
-            /*
+
         list.addListSelectionListener(new ListSelectionListener() {
 
             public void valueChanged(ListSelectionEvent event) {
                 if (!event.getValueIsAdjusting()){
                     JList source = (JList)event.getSource();
                     String selected = source.getSelectedValue().toString();
+                    // TEST
+                    System.out.println(selected);
                 }
             }
 
-        }); */
+        });
 
         frame.setVisible(true);
 
--- main_module/src/main/java/com/asdmorning3/test/main.java
+++ main_module/src/main/java/com/asdmorning3/test/main.java
@@ -1,16 +1,19 @@
 package com.asdmorning3.test;
 
+
 import javax.swing.*;
+import com.asdmorning3.basic.studyInterface;
 import com.asdmorning3.basic.Vocable;
 import com.asdmorning3.basic.VocableDictionary;
-
-
 public class main {
 
 	public static void main (String[] args)
 	{
 
-		studyInterface Interface = new studyInterface();
+		VocableDictionary dictionary = new VocableDictionary();
+		dictionary.addVocable(new Vocable("hallo", Vocable.Language.GER),
+				new Vocable("hello", Vocable.Language.ENG));
+		studyInterface Interface = new studyInterface(dictionary);
 
 	}
 
